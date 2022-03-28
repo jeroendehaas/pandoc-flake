c93413f5c5071a22a8172c1d1e58009829c8b50d
Jeroen de Haas <jah.dehaas@avans.nl>
2022-03-28 20:34:43 +0200

    Flake for building pandoc documents

---
+++ flake.nix
@@ -0,0 +1,60 @@
+# Copyright (c) 2022, Jeroen de Haas
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+{
+  description = "A template for building documents using Pandoc";
+
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, nixpkgs, flake-utils, ... }: {
+    forDocs = { targets ? {},
+                fonts ? (pkgs: []),
+                extraBuildInputs ? (pkgs: [])}: system:
+      let pkgs = import nixpkgs { inherit system; };
+          texlive-combined = with pkgs; [(texlive.combine {
+            inherit (texlive) scheme-medium collection-latexextra;
+          })];
+          buildInputs = (with pkgs; [
+            texlive-combined
+            gnumake
+            pandoc
+            haskellPackages.pandoc-crossref
+          ]) ++ (extraBuildInputs pkgs);
+          selectedFonts = fonts pkgs;
+      in {
+        devShell = pkgs.mkShell {
+          inherit buildInputs;
+        };
+        packages = builtins.mapAttrs (name: {target, files, path}: pkgs.stdenvNoCC.mkDerivation {
+          inherit name;
+          inherit buildInputs;
+          src = builtins.path { inherit path; inherit name; };
+          buildPhase = ''
+            env OSFONTDIR=${builtins.concatStringsSep ":" selectedFonts} TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var make ${target}
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp -r ${builtins.concatStringsSep " " files} $out
+          '';
+        }) targets;
+      };
+  };
+}
